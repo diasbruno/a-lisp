b647b1d02bd8c426360540e6a05503f2bd15f987
Bruno Dias <dias.h.bruno@gmail.com>
2014-11-11 22:57:34 -0200

    evaluating number.

--- tests/units/test_eval.cc
+++ tests/units/test_eval.cc
@@ -10,6 +10,16 @@ TEST_CASE("eval null.", "[eval]") {
   REQUIRE(!r);
 }
 
+TEST_CASE("eval number.", "[eval]") {
+  lisp_t* n = lisp_number(10);
+  lisp_t* r = lisp_eval(n, NULL);
+
+  REQUIRE(L_IS_NUMBER(n));
+  REQUIRE(with_number(r).nvalue == 10);
+
+  free((void*)n);
+}
+
 int main(int argc, char* const argv[])
 {
   // initialize the local_env with lisp 'nil'.
